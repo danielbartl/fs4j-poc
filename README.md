b74dc37f998aa409cbfbdb04483bdac30cedf95a
Daniel Bartl <630704+danielbartl@users.noreply.github.com>
2025-11-01 10:45:16 +0100

    Initialize Fullstack4J PoC project with Spring Boot, Apache Wicket, Spring Modulith, and EclipseStore integration.

---
+++ README.md
@@ -0,0 +1,6 @@
+# Fullstack4J Proof of Concept
+
+Welcome to the PoC for full-stack java development
+with Java 25, Apache Wicket 10, Spring Boot 4, Spring Modulith 2 and EclipseStore 3.
+        
+May the force be with you!
---
+++ src/main/java/dev/fullstack4j/poc/DemoApplication.java
@@ -0,0 +1,51 @@
+package dev.fullstack4j.poc;
+
+import org.apache.wicket.Page;
+import org.apache.wicket.protocol.http.WebApplication;
+import org.apache.wicket.protocol.http.WicketFilter;
+import org.apache.wicket.spring.injection.annot.SpringComponentInjector;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.Bean;
+
+@SpringBootApplication
+public class DemoApplication {
+
+    static void main(String[] args) {
+        SpringApplication.run(DemoApplication.class, args);
+    }
+
+    @Bean
+    WebApplication wicketApp(ApplicationContext ctx) {
+
+        return new WebApplication() {
+
+            @Override
+            public Class<? extends Page> getHomePage() {
+
+                return HomePage.class;
+
+            }
+
+            @Override
+            protected void init() {
+
+                super.init();
+                getComponentInstantiationListeners().add(
+                        new SpringComponentInjector(this, ctx));
+
+            }
+        };
+
+    }
+
+    @Bean
+    WicketFilter wicketFilter(WebApplication wicketApp) {
+
+        final WicketFilter filter = new WicketFilter(wicketApp);
+        filter.setFilterPath("/");
+        return filter;
+
+    }
+}
