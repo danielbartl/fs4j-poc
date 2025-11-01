cfa8298c0ba160feb3e902f3904e815feb9309f4
Daniel Bartl <630704+danielbartl@users.noreply.github.com>
2025-11-01 11:18:53 +0100

    Make `main` method public and update Java version to 21 in `pom.xml`.

--- src/main/java/dev/fullstack4j/poc/DemoApplication.java
+++ src/main/java/dev/fullstack4j/poc/DemoApplication.java
@@ -12,7 +12,7 @@ import org.springframework.context.annotation.Bean;
 @SpringBootApplication
 public class DemoApplication {
 
-    static void main(String[] args) {
+    public static void main(String[] args) {
         SpringApplication.run(DemoApplication.class, args);
     }
 
