6fbf61e4d54b2da822c936b2b9f03ec055591205
Daniel Bartl <630704+danielbartl@users.noreply.github.com>
2025-11-04 16:26:52 +0100

    Enhance UI with Tailwind CSS and DaisyUI, integrate CSP settings, and update HomePage layout.

--- src/main/java/dev/fullstack4j/poc/HomePage.java
+++ src/main/java/dev/fullstack4j/poc/HomePage.java
@@ -1,6 +1,18 @@
 package dev.fullstack4j.poc;
 
+import org.apache.wicket.markup.head.CssHeaderItem;
+import org.apache.wicket.markup.head.IHeaderResponse;
+import org.apache.wicket.markup.head.JavaScriptHeaderItem;
 import org.apache.wicket.markup.html.WebPage;
 
 public class HomePage extends WebPage {
+
+    @Override
+    public void renderHead(IHeaderResponse response) {
+        super.renderHead(response);
+
+        response.render(JavaScriptHeaderItem.forUrl("https://cdn.jsdelivr.net/npm/@tailwindcss/browser@4"));
+        response.render(CssHeaderItem.forUrl("https://cdn.jsdelivr.net/npm/daisyui@5"));
+
+    }
 }
